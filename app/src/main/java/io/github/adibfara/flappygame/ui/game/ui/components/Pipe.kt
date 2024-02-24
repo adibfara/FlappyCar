a82808ea8d4e48b5211e209c3c5f4b8460baf04f
Adib Faramarzi <adibfara@gmail.com>
2024-02-24 12:57:48 +0330

    Update image for blocks

--- app/src/main/java/io/github/adibfara/flappygame/ui/game/ui/components/Pipe.kt
+++ app/src/main/java/io/github/adibfara/flappygame/ui/game/ui/components/Pipe.kt
@@ -1,5 +1,6 @@
 package io.github.adibfara.flappygame.ui.game.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
@@ -7,19 +8,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import io.github.adibfara.flappygame.R
 import io.github.adibfara.flappygame.ui.game.model.Pipe
 
 
 @Composable
 internal fun Pipe(pipe: Pipe, modifier: Modifier = Modifier) {
-    Box(
+    Image(
+        painterResource(id = R.drawable.pipe),
+        contentDescription = null,
         modifier
             .offset {
                 IntOffset(pipe.x.dp.roundToPx(), pipe.topY.dp.roundToPx())
             }
             .size(pipe.width.dp, (pipe.bottomY - pipe.topY).dp)
-            .background(Color.Green)
+            .background(Color.Green),
+        contentScale = ContentScale.FillBounds
     )
 }
